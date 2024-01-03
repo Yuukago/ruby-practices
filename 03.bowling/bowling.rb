7dda69cadf683c42c27c134473cdb3135e68d9a0
Yuukago <yuka.ago.work@gmail.com>
2024-01-03 18:03:58 +0900

    正しいスコアになるように修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+def calculate_strike_points(next_frame, after_next_frame)
+  if next_frame[0] == 10 && after_next_frame[0] == 10
+    30
+  elsif next_frame[0] == 10
+    20 + after_next_frame[0]
+  else
+    10 + next_frame.sum
+  end
+end
+
 scores = ARGV[0].split(',')
-# 引数を数字に変換する。
 shots = []
 scores.each do |s|
   if s == 'X'
@@ -11,40 +20,20 @@ scores.each do |s|
     shots << s.to_i
   end
 end
+frames = shots.each_slice(2).to_a
 
-# １フレームごとに配列を作る。
-flames = []
-shots.each_slice(2) do |s|
-  flames << s
-end
-
-# ストライクの場合、スペアの場合で加算されるように書く。
 point = 0
-flames.each_with_index do |frame, index|
-  next_flame = flames[index + 1]
-  third_frame = flames[index + 2]
-  if frame[0] == 10 # ストライク
-    # 10フレーム目がすべてXだった場合
-    if index == 9 && next_flame[0] == 10 && third_frame[0] == 10
-      point += frame.sum + 20
-      break
-    # 最後の投球がストライクだった、もしくは10フレーム目の1投目がストライクの場合
-    elsif index == flames.length - 1 || index == 9
-      point += 10
-    # ストライクが２回以上続いた場合
-    elsif next_flame[0] == 10
-      point += frame.sum + 10 + third_frame[0]
-    # ストライクが1回
-    else
-      point += frame.sum + next_flame.sum
-    end
-
-  elsif frame.sum == 10 # スペア
-    # 10フレーム目がスペアだった場合
+frames.each_with_index do |frame, index|
+  next_frame = frames[index + 1]
+  after_next_frame = frames[index + 2]
+  if frame[0] == 10
+    point += calculate_strike_points(next_frame, after_next_frame)
+    break if index >= 9
+  elsif frame.sum == 10
     point += if index >= 9
                frame.sum
              else
-               frame.sum + next_flame[0]
+               frame.sum + next_frame[0]
              end
   else
     point += frame.sum
