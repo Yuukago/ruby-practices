bd9ec73cf460a35e0d537865c193777006ef9959
Yuukago <yuka.ago.work@gmail.com>
2024-05-01 19:50:13 +0900

    ls.rbを修正、不要なファイルを削除

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 def file_list(column)
-  files = Dir.glob('*').sort
+  files = Dir.glob('*')
   surplus = files.size % column
-  if surplus == 1
-    files.push('', '')
-  elsif surplus == 2
-    files.push('')
-  end
+  (column - surplus).times { files.push('') } unless surplus.zero?
   files.each_slice(files.size / column).to_a.transpose
 end
 
 def display_files(file)
+  max_widths = file.flatten.map(&:size).max + 8
+
   file.each do |array|
     array.each do |element|
-      print element.ljust(24)
+      print element.ljust(max_widths)
     end
-    print "\n"
+    puts
   end
 end
 
